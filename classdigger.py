81538402ceaebb01628e4ab89ef74db9d95b2042
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2015-08-09 10:25:12 +0300

    html support

--- classdigger.py
+++ classdigger.py
@@ -10,6 +10,13 @@ class ClassDigger(object):
     def is_excluded(self, name):
         return name.startswith('__')
 
+    def get_file(self, member):
+        try:
+            f = inspect.getabsfile(member)
+        except TypeError:
+            f = None
+        return f
+
     def get_structure(self, cls):
         supers = list(inspect.getmro(cls))
         supers.pop(0)
@@ -17,7 +24,10 @@ class ClassDigger(object):
         for name, member in inspect.getmembers(cls):
             if self.is_excluded(name):
                 continue
-            structure_member = {'obj': member, 'name': name}
+            structure_member = {
+                'obj': member,
+                'name': name,
+                'file': self.get_file(member)}
             try:
                 structure_member['lines'] = inspect.getsourcelines(
                     member)
@@ -33,24 +43,21 @@ class ClassDigger(object):
                     if _sname == name and _smember == member:
                         structure_member['root'] = {
                             'obj': _smember,
-                            'cls': _super}
-                        try:
-                            f = inspect.getabsfile(_super)
-                            structure_member['root']['file'] = f
-                        except TypeError:
-                            structure_member['root']['file'] = None
+                            'cls': _super,
+                            'file': self.get_file(_super)}
             if structure_member['type'] == 'attr':
                 structure['members'].insert(0, structure_member)
             else:
                 structure['members'].append(structure_member)
+        inherited = map(lambda x: x.__name__, structure['supers'])
+        if len(inherited) > 1:
+            inherited.pop(inherited.index('object'))
+        structure['inherited'] = 'class %s(%s):\n' % (
+            structure['cls'].__name__, ','.join(inherited))
         return structure
 
     def as_text(self):
-        inherited = map(lambda x: x.__name__, self.structure['supers'])
-        if len(inherited) > 1:
-            inherited.pop(inherited.index('object'))
-        txt = 'class %s(%s):\n' % (
-            self.structure['cls'].__name__, ','.join(inherited))
+        txt = self.structure['inherited']
         for member in self.structure['members']:
             lines = member['lines'][0][:]
             if member.get('root'):
@@ -62,11 +69,19 @@ class ClassDigger(object):
 
         return txt
 
-    def write(self, path, overwrite=False):
+    def as_html(self):
+        from jinja2 import Template
+        template = Template(open('template.html', 'r').read())
+        return template.render(structure=self.structure)
+
+    def write(self, path, overwrite=False, contents=None):
         if not overwrite and os.path.exists(path):
             raise IOError('File exists!')
         with open(path, 'w') as f:
-            f.write(self.as_text())
+            if not contents:
+                f.write(self.as_text())
+            else:
+                f.write(contents)
 
 
 if __name__ == '__main__':
@@ -75,5 +90,7 @@ if __name__ == '__main__':
     parser = ClassDigger(D)
     pprint(parser.structure)
     print('\n\n')
-    print(parser.as_text())
+    print(parser.as_html())
     parser.write('examples/results.py', overwrite=True)
+    parser.write('examples/results.html', overwrite=True,
+                 contents=parser.as_html())
